77f2e392d6c5c243aff6a60286dfcd3aab903186
Emrah Atalay <emrahatalay92@gmail.com>
2022-01-15 23:18:42 +0300

    Initial Commit

---
+++ ImdbMovieApp/ImdbMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImdbMovieAppApp.swift
+//  ImdbMovieApp
+//
+//  Created by Emrah Atalay on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ImdbMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
